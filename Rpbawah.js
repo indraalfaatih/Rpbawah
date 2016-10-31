75acb7da40553ea02c0e1752f64c4781ef3b95ed
indraalfaatih <indraalfaatih@gmail.com>
2016-10-31 18:48:20 +0700

    Add files via upload

---
+++ Rpbawah.js
@@ -0,0 +1,5 @@
+var currentposturl=&quot;<data:post.url/>&quot;;
+var maxresults=8;
+var relatedpoststitle=&quot;<b>Related Posts:</b>&quot;;
+removeRelatedDuplicates_thumbs();
+printRelatedLabels_thumbs();
